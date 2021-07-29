f02b817ba3316e74450fcc8f45cf4d6e8dd7ccf9
Kartik <kjmickey003@gmail.com>
2021-07-29 22:57:05 +0530

    Add Initial files

--- Task2/app.js
+++ Task2/app.js
@@ -1,13 +1,52 @@
 //jshint esversion: 6
 
+// Requiring npm modules
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
 
+// Home route for all the characters in Breaking Bad
 app.get("/", function (req, res) {
-  res.send("Running...");
-});
-app.post("/");
+  // Using axios to get data from their api
+  axios
+    .get("https://www.breakingbadapi.com/api/characters")
 
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+      res.send("Success");
+    })
+
+    .catch(function (error) {
+      // handle error
+      res.send("Error");
+      console.log(error);
+    });
+});
+
+// /betterCallSaul for all the character in Better Call Saul
+app.get("/betterCallSaul", function (req, res) {
+  // Using axios to get data from their api
+  axios
+    .get(
+      "https://www.bdreakingbadapi.com/api/characters?category=Better+Call+Saul"
+    )
+
+    .then(function (response) {
+      // handle success
+      res.send("Success");
+      console.log(response.data);
+    })
+
+    .catch(function (error) {
+      // handle error
+      res.send("Error");
+      console.log(error);
+    });
+});
+
+// Setting sever to listen on port 3000
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
